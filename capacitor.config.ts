3e3876bca43b50043b34d132e974025b368e44d6
Jean10112002 <jeanrodriguez060420@gmail.com>
2024-07-02 22:35:05 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-Agenda-Ionic',
+  webDir: 'www'
+};
+
+export default config;
