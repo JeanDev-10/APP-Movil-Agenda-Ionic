1b7d25dcb4eb2c951b3f77aa7798eb2410478b4a
Jean10112002 <jeanpierrerodriguezzambrano@gmail.com>
2024-07-05 10:52:24 -0500

    style: login page and register ready

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,20 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  arrowForwardCircle,
+  eyeOffOutline,
+  eyeOutline,
+  personAddOutline,
+} from 'ionicons/icons';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
+import { ToastService } from 'src/app/core/services/toast.service';
+import { addIcons } from 'ionicons';
+import { RouterModule } from '@angular/router';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    NavbarComponent,
+    ReactiveFormsModule,
+    RouterModule,
+    LottieComponent,
+  ],
 })
 export class LoginPage implements OnInit {
+  /**
+   * ?Inyectando servicios
+   */
+  private toastService = inject(ToastService);
+  private fb = inject(FormBuilder);
 
-  constructor() { }
+  /**
+   * ? CONFIGURACIÓN ANIMACIONES
+   */
+  options: AnimationOptions = {
+    path: '/assets/animations/register-login.json',
+  };
 
-  ngOnInit() {
+  styles = {
+    width: '50%',
+    height: 'auto',
+    margin: '0 auto',
+    marginTop: '2em',
+  };
+
+  /**
+   * ?Variables
+   */
+  loginForm!: FormGroup;
+  IsPassword: boolean = false;
+  constructor() {
+    this.registerIcons();
+    this.registerLoginForm();
   }
 
+  ngOnInit() {}
+  SeePassword() {
+    this.IsPassword = !this.IsPassword;
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      /**
+       * ! ENVIAR PETICIÓN HTTP
+       *  */
+      console.log(this.loginForm.value);
+    } else {
+      /**
+       * ! MOSTRAR TOAST DE FORMULARIO INVALIDO
+       *  */
+      this.toastService.presentToastError('¡Formulario invalido!');
+    }
+  }
+
+  getformHasError(field: string, rule: string): boolean | undefined {
+    return this.loginForm.get(field)?.hasError(rule);
+  }
+  getFormHasTouch(field: string): boolean | undefined {
+    return this.loginForm.get(field)?.touched;
+  }
+  private registerIcons() {
+    addIcons({
+      eyeOffOutline,
+      eyeOutline,
+      personAddOutline,
+      arrowForwardCircle,
+    });
+  }
+  private registerLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(10),
+        ],
+      ],
+    });
+  }
 }
--- src/app/auth/pages/register/register.page.ts
+++ src/app/auth/pages/register/register.page.ts
@@ -1,54 +1,83 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
 import { addIcons } from 'ionicons';
-import { add, eye, eyeOffOutline, eyeOutline, lockClosed, personAddOutline } from 'ionicons/icons';
+import {
+  add,
+  arrowForwardCircle,
+  eye,
+  eyeOffOutline,
+  eyeOutline,
+  lockClosed,
+  personAddOutline,
+} from 'ionicons/icons';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { passwordMatchValidator } from '../../../core/helpers/validators';
 import { ToastService } from 'src/app/core/services/toast.service';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [  CommonModule, FormsModule,NavbarComponent,IonicModule,RouterModule,ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    NavbarComponent,
+    IonicModule,
+    RouterModule,
+    ReactiveFormsModule,
+    LottieComponent,
+  ],
 })
 export class RegisterPage implements OnInit {
-  registerForm: FormGroup;
-  IsPassword:boolean=false;
-  IsConfirmationPassword:boolean=false;
-  private toastService=inject(ToastService);
+  /**
+   * ?Inyectando servicios
+   */
+  private toastService = inject(ToastService);
+  private fb = inject(FormBuilder);
+  /**
+   * ? CONFIGURACIÓN ANIMACIONES
+   */
+  options: AnimationOptions = {
+    path: '/assets/animations/register-login.json',
+  };
 
-  constructor(private fb: FormBuilder) {
-    addIcons({
-      eyeOffOutline,
-      eyeOutline,
-      lockClosed,
-      eye,
-      add,
-      personAddOutline
-    })
-    this.registerForm = this.fb.group({
-      firstname: ['', [Validators.required, Validators.minLength(3)]],
-      lastname: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
-    },{
-      validators: passwordMatchValidator
-    });
+  styles = {
+    width: '50%',
+    height: 'auto',
+    margin: '0 auto',
+    marginTop: '2em',
+  };
+
+  /**
+   * ?Variables
+   */
+  registerForm!: FormGroup;
+  IsPassword: boolean = false;
+  IsConfirmationPassword: boolean = false;
+
+  constructor() {
+    this.registerIcons();
+    this.registerRegisterForm();
+
   }
 
-  ngOnInit() {
-  }
-  SeePassword(text:string){
-    if(text=='password'){
-      this.IsPassword=!this.IsPassword;
-    }else{
-      this.IsConfirmationPassword=!this.IsConfirmationPassword;
+  ngOnInit() {}
+  SeePassword(text: string) {
+    if (text == 'password') {
+      this.IsPassword = !this.IsPassword;
+    } else {
+      this.IsConfirmationPassword = !this.IsConfirmationPassword;
     }
   }
   onSubmit() {
@@ -61,14 +90,53 @@ export class RegisterPage implements OnInit {
       /**
        * ! MOSTRAR TOAST DE FORMULARIO INVALIDO
        *  */
-      this.toastService.presentToastError("¡Formulario invalido!")
+      this.toastService.presentToastError('¡Formulario invalido!');
     }
   }
 
-  getformHasError(field:string,rule:string){
+  getformHasError(field: string, rule: string): boolean | undefined {
     return this.registerForm.get(field)?.hasError(rule);
   }
-  getFormHasTouch(field:string){
+  getFormHasTouch(field: string): boolean | undefined {
     return this.registerForm.get(field)?.touched;
+  }
+  private registerIcons(){
+    addIcons({
+      eyeOffOutline,
+      eyeOutline,
+      lockClosed,
+      arrowForwardCircle,
+      eye,
+      add,
+      personAddOutline,
+    });
+  }
+  private registerRegisterForm(){
+    this.registerForm = this.fb.group(
+      {
+        firstname: ['', [Validators.required, Validators.minLength(3)]],
+        lastname: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(10),
+          ],
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(10),
+          ],
+        ],
+      },
+      {
+        validators: passwordMatchValidator,
+      }
+    );
   }
 }
