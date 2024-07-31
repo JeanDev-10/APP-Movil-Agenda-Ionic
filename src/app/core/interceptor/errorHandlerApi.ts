21d4d37405b9635eb0810b3a9310dd441c05c8df
jeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-07-31 11:12:53 -0500

    feat: :fire: handle error token invalid or expired

--- src/app/core/interceptor/errorHandlerApi.ts
+++ src/app/core/interceptor/errorHandlerApi.ts
@@ -1,11 +1,22 @@
 import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { ToastService } from '../services/toast.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { Router } from '@angular/router';
 export const ErrorApiInterceptorFn: HttpInterceptorFn = (req, next) => {
 	console.log('---ErrorApiInterceptorFn', req);
+  const _toastService=inject(ToastService);
+  const _localStorageService=inject(LocalStorageService);
+  const _router=inject(Router);
 
 	return next(req).pipe(
 		catchError((error: HttpErrorResponse) => {
 			if (error.status === HttpStatusCode.Unauthorized) {
+        _localStorageService.deleteToken();
+        _toastService.presentToastError("¡La sesión ha finalizado!");
+        _router.navigateByUrl('/auth/login');
+
         console.error("no tienes acceso")
 			} else {
 				console.error("hay un error", error)
