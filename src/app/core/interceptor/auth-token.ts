e570e09b3aeae68ba454f6450668b48c6b6b1a2d
jeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-07-31 12:48:21 -0500

    feat: :fire: refresh token ready

--- src/app/core/interceptor/auth-token.ts
+++ src/app/core/interceptor/auth-token.ts
@@ -1,26 +1,72 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
+import { UserService } from '../services/user.service';
+import { ResponseCommon } from '../models/User.model';
+import { Router } from '@angular/router';
+import { ToastService } from '../services/toast.service';
 
 @Injectable()
 export class authTokenInterceptor implements HttpInterceptor {
-	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		if (req.url.includes('white_')) {
-			const requestClone = req.clone({ url: this.cleanWhiteUrl(req.url) });
-			return next.handle(requestClone);
-		}
-    const token=localStorage.getItem('token');
+  private _localStorageService = inject(LocalStorageService);
+  private _userService = inject(UserService);
+  private _router = inject(Router);
+  private _toastService = inject(ToastService);
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (req.url.includes('white_')) {
+      const requestClone = req.clone({ url: this.cleanWhiteUrl(req.url) });
+      return next.handle(requestClone);
+    }
+    const token = localStorage.getItem('token');
     let request = req;
     request = req.clone({
       setHeaders: {
         authorization: `Bearer ${token}`,
       },
     });
-    console.log(request)
-		return next.handle(request);
-	}
+    console.log(request);
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if(err.status==401&&(err.message=="Token expirado"||"Token invalido")){
+          return this._userService.refreshToken().pipe(
+            switchMap((res) => {
+              this._localStorageService.setToken(res.data);
+              const token=res.data;
+              const newReq = req.clone({
+                setHeaders: {
+                  authorization: `Bearer ${token}`,
+                },
+              });
+              return next.handle(newReq);
+            }),
+            catchError((err) => {
+              const finalError = new Error();
+              this._localStorageService.deleteToken();
+              this._toastService.presentToastError("¡Sesión finalizada!")
+              this._router.navigateByUrl('/auth/login');
+              return throwError(() => finalError);
+            })
+          );
+        }
+        return throwError(() => err);
+      })
+    );
+  }
 
-	private cleanWhiteUrl(url: string) {
-		return url.replace('white_', '');
-	}
+  private cleanWhiteUrl(url: string) {
+    return url.replace('white_', '');
+  }
 }
+
+
+
--- src/app/core/interceptor/errorHandlerApi.ts
+++ src/app/core/interceptor/errorHandlerApi.ts
@@ -1,27 +1,26 @@
-import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import {
+  HttpErrorResponse,
+  HttpInterceptorFn,
+  HttpStatusCode,
+} from '@angular/common/http';
+import { catchError, switchMap, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { ToastService } from '../services/toast.service';
 import { LocalStorageService } from '../services/local-storage.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/service/auth.service';
+import { UserService } from '../services/user.service';
 export const ErrorApiInterceptorFn: HttpInterceptorFn = (req, next) => {
-	console.log('---ErrorApiInterceptorFn', req);
-  const _toastService=inject(ToastService);
-  const _localStorageService=inject(LocalStorageService);
-  const _router=inject(Router);
+  console.log('---ErrorApiInterceptorFn', req);
+  const _toastService = inject(ToastService);
+  const _localStorageService = inject(LocalStorageService);
+  const _userService = inject(UserService);
+  const _router = inject(Router);
 
-	return next(req).pipe(
-		catchError((error: HttpErrorResponse) => {
-			if (error.status === HttpStatusCode.Unauthorized) {
-        _localStorageService.deleteToken();
-        _toastService.presentToastError("¡La sesión ha finalizado!");
-        _router.navigateByUrl('/auth/login');
-
-        console.error("no tienes acceso")
-			} else {
-				console.error("hay un error", error)
-			}
-			return throwError(() => error);
-		})
-	);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      console.error('hay un error', error);
+      return throwError(() => error);
+    })
+  );
 };
