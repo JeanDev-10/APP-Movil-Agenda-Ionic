4aa42f484a56c1b496e1751de4aa8c0a1b84dc7f
jeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-08-02 16:06:06 -0500

    fix: :bug: auth with no token

--- src/app/core/resolver/Contacts/ContactDetailResolver.resolver.ts
+++ src/app/core/resolver/Contacts/ContactDetailResolver.resolver.ts
@@ -4,19 +4,21 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@a
 import { catchError, Observable, throwError } from 'rxjs';
 import { ContactService } from '../../services/contact.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ToastService } from '../../services/toast.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactDetailResolver implements Resolve<ContactShowI> {
 
-  constructor(private contactService: ContactService,private router:Router) {}
+  constructor(private contactService: ContactService,private router:Router,private readonly _toastService:ToastService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ContactShowI> {
     const id = route.paramMap.get('id');
     return this.contactService.getContactById(id).pipe(
       catchError((err:HttpErrorResponse)=>{
         if(err.status==500){
+          this._toastService.presentToastError("Error del servidor!")
           this.router.navigate(['/dashboard/contacts']);
         }
         return throwError(() => err);
